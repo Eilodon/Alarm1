f23d358c8c96e88a5f9021238328ca381b131fb4
Ybao <bao.nt.1992@gmail.com>
2025-08-29 23:58:19 +0700

    Add Lottie assets và SettingsService

---
+++ lib/services/settings_service.dart
@@ -0,0 +1,28 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+class SettingsService {
+  static const _kThemeColor = 'theme_color';
+  static const _kMascotPath = 'mascot_path';
+
+  Future<void> saveThemeColor(Color color) async {
+    final sp = await SharedPreferences.getInstance();
+    await sp.setInt(_kThemeColor, color.value);
+  }
+
+  Future<Color> loadThemeColor() async {
+    final sp = await SharedPreferences.getInstance();
+    final v = sp.getInt(_kThemeColor);
+    return v != null ? Color(v) : Colors.blue;
+  }
+
+  Future<void> saveMascotPath(String path) async {
+    final sp = await SharedPreferences.getInstance();
+    await sp.setString(_kMascotPath, path);
+  }
+
+  Future<String> loadMascotPath() async {
+    final sp = await SharedPreferences.getInstance();
+    return sp.getString(_kMascotPath) ?? 'assets/lottie/mascot.json';
+  }
+}
