e924857d419c5b0b6fad664d42f1cd63733355f2
Eilodon <bao.nt.1992@gmail.com>
2025-09-05 19:37:43 +0700

    Add integration test setup

---
+++ integration_test/app_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:notes_reminder_app/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('add and delete note and verify navigation', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+
+    app.main();
+    await tester.pumpAndSettle();
+
+    expect(find.text('No notes yet'), findsOneWidget);
+
+    await tester.tap(find.byTooltip('Add note'));
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+        find.widgetWithText(TextFormField, 'Title'), 'Test Note');
+    await tester.enterText(
+        find.widgetWithText(TextFormField, 'Content'), 'Sample content');
+
+    await tester.tap(find.text('Save'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Test Note'), findsOneWidget);
+
+    await tester.tap(find.text('Test Note'));
+    await tester.pumpAndSettle();
+    expect(find.text('Read Note'), findsOneWidget);
+
+    await tester.pageBack();
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byTooltip('Delete'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('No notes yet'), findsOneWidget);
+  });
+}
---
+++ integration_test/flutter_test_config.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+import 'package:integration_test/integration_test.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  await testMain();
+}
