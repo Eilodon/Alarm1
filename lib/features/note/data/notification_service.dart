1c5282b542269364ad4619e0384ee09237d6e19a
Eilodon <bao.nt.1992@gmail.com>
2025-09-08 03:02:01 +0700

    chore: replace native timezone plugin

--- lib/features/note/data/notification_service.dart
+++ lib/features/note/data/notification_service.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart' as fln;
 import 'package:notes_reminder_app/generated/app_localizations.dart';
-import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
 
 import 'package:timezone/data/latest.dart' as tzdata;
 import 'package:timezone/timezone.dart' as tz;
