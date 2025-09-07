00ab1988de513a9f515c9ebe40d97f43ea105d27
Eilodon <bao.nt.1992@gmail.com>
2025-09-08 04:55:41 +0700

    chore: add alarm_data import

--- lib/features/settings/presentation/settings_screen.dart
+++ lib/features/settings/presentation/settings_screen.dart
@@ -7,6 +7,7 @@ import 'dart:math' as math;
 
 import '../domain/settings_service.dart';
 
+import 'package:alarm_data/alarm_data.dart';
 import 'package:alarm_domain/alarm_domain.dart';
 
 import 'package:provider/provider.dart';
