7d6d37c0b90eea9f3043576b7c40eaecb0b8ac6c
Eilodon <bao.nt.1992@gmail.com>
2025-09-07 22:47:26 +0700

    refactor: remove unused backup service

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,6 @@ import '../features/settings/presentation/settings_screen.dart';
 import '../features/note/presentation/voice_to_note_screen.dart';
 import '../features/settings/data/settings_service.dart';
 
-import '../features/backup/data/backup_service.dart';
 import 'package:alarm_domain/alarm_domain.dart';
 
 import '../pandora_ui/palette_bottom_sheet.dart';
@@ -50,8 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
           onThemeChanged: widget.onThemeChanged,
           onFontScaleChanged: widget.onFontScaleChanged,
           onThemeModeChanged: widget.onThemeModeChanged,
-          settingsService:
-              SettingsServiceImpl(backupService: BackupServiceImpl()),
+          settingsService: SettingsServiceImpl(),
         ),
       ];
   }
