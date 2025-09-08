2558636b33796eb93120025cec6564e57d5a023d
Eilodon <bao.nt.1992@gmail.com>
2025-09-08 21:47:35 +0700

    ok

--- android/app/src/main/kotlin/com/pandora/core/PandoraApp.kt
+++ android/app/src/main/kotlin/com/pandora/core/PandoraApp.kt
@@ -3,5 +3,9 @@ package com.pandora.core
 import android.app.Application
 
 class PandoraApp : Application() {
-    // Custom global app logic (if needed)
+    override fun onCreate() {
+        super.onCreate()
+        // Khởi tạo các thành phần toàn cục nếu cần
+        android.util.Log.d("PandoraApp", "PandoraApp started")
+    }
 }
